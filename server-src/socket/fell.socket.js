6cde086799dccd11dbaf61ed58aff1795ead059e
Josh Chansard <josh.chansard@gmail.com>
2017-04-29 06:18:56 +0100

    use FellEvents in server

--- server-src/socket/fell.socket.js
+++ server-src/socket/fell.socket.js
@@ -1,25 +1,34 @@
-module.exports = (io) => {
+const FellEvents = require('../shared/tree-response').FellEvents;
+let fellEvents = new FellEvents();
 
+let mockTree = {
+    id: 1,
+    color: 'brown',
+    pointData: [
+      [5, 5],
+      [5, 900],
+      [305, 900],
+      [305, 5]
+    ]
+}
+
+// tree response handler: emit a response event with a tree JSON
+let treeResponse = (io) => (responseEvent) => {
+  console.log('tree requested');
+  io.json.emit(responseEvent, mockTree);
+}
+
+// sets up the passed IO to respond to tree request events
+module.exports = (io) => {
+  // todo: move this to its own module; figure out how to alter events after on(connection)
   io.on('connection', (socket) => {
     console.log('user connected');
 
     socket.on('disconnect', () => console.log('user disconnected'));
 
-    socket.on('tree-request', () => {
-      console.log('tree requested');
-      io.json.emit('tree-update', {
-          id: 1,
-          color: 'brown',
-          pointData: [
-            [5, 5],
-            [5, 900],
-            [305, 900],
-            [305, 5]
-          ]
-        }
-      )
+    fellEvents.treeRequestStream().subscribe(requestEvent => {
+      socket.on(requestEvent, () => fellEvents.treeResponseStream().subscribe(treeResponse(io)));
     });
   });
-
   return io;
 }
