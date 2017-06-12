72b1bc64efc3bdbacf3114a3c4c94ce39b773013
Josh Chansard <josh.chansard@gmail.com>
2017-06-12 11:21:17 +0100

    quit script when done

--- server-src/db/scripts/build.js
+++ server-src/db/scripts/build.js
@@ -1,6 +1,11 @@
 let Database = require('../db.js');
 let db = new Database();
 
+const Observable = require('rxjs/Observable').Observable;
+require('rxjs/add/operator/elementAt');
+require('rxjs/add/operator/merge');
+require('rxjs/add/operator/map');
+
 let players = [
   {
     playerID: 0,
@@ -18,7 +23,7 @@ let players = [
     deck: [5,5,8]
   },
   {
-    playerID: 3, // todo: link to encoded id?
+    playerID: 3, // todo: link to encoded?
     commander: 2,
     deck: [6,7,5]
   }
@@ -26,5 +31,16 @@ let players = [
 
 let users = ["glasg0wn3d","OXBOW","online_playing","LEFT4SCRAPS"];
 
-players.forEach((player) => db.players.add(JSON.stringify(player)));
-users.forEach((user) => db.users.add(user));
+// add each player
+let addPlayersStream = new Observable((stream) => {
+  players.forEach((player) => db.players.add(JSON.stringify(player)).subscribe(() => stream.next()));
+}).elementAt(players.length-1).map(() => console.log("players added"));
+
+// add each user
+let addUsersStream = new Observable((stream) => {
+  users.forEach((user) => db.users.add(user).subscribe(() => stream.next()));
+}).elementAt(users.length-1).map(() => console.log("users added"));;
+
+// exit when done
+let doneStream = addPlayersStream.merge(addUsersStream).elementAt(1).map(() => console.log("done"));
+doneStream.subscribe(process.exit);
